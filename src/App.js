76f6b5f7f7b5567add060fba563b3f0ec8bac351
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-06-16 23:52:10 +0200

    2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>deploy test</p>
+        <p>deploy test 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
